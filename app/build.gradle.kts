6b8f411c5b90986a59e5b66e67d7143429da3e9d
fjzamora93 <fjzamora93@gmail.com>
2024-12-24 14:18:21 -0300

    actualizacion documentacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.kotlinplotgenerator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.kotlinplotgenerator"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
